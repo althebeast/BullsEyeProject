5c717960a916cb0376057c236f5fca392590835f
althebeast <88689564+althebeast@users.noreply.github.com>
2023-05-24 17:14:13 -0400

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Alperen Sarışan on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
